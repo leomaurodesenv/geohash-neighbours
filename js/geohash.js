e3454ddbfb7675eba7f5b4c443d7da13668dd05b
Leonardo Moraes <leo.mauro.desenv@gmail.com>
2020-06-01 21:58:37 -0300

    add html layout

---
+++ js/geohash.js
@@ -0,0 +1,13 @@
+/**
+ * @module Geohash
+ * This class extracts the geohash code from a latitude and longitude.
+ * 
+ * @author Leonardo Mauro <leo.mauro.desenv@gmail.com> (http://leonardomauro.com/)
+ * @link https://github.com/leomaurodesenv/geohash-neighbour GitHub
+ * @copyright 2020 Leonardo Mauro
+ * @package geohash-neighbour
+ * @access public
+ */
+class Geohash {
+	nome = "Test"
+}
---
+++ js/openstreetmap.js
@@ -0,0 +1,13 @@
+/**
+ * @module OpenStreetMap
+ * This class process OpenStreetMap iframe from a latitude and longitude.
+ * 
+ * @author Leonardo Mauro <leo.mauro.desenv@gmail.com> (http://leonardomauro.com/)
+ * @link https://github.com/leomaurodesenv/geohash-neighbour GitHub
+ * @copyright 2020 Leonardo Mauro
+ * @package geohash-neighbour
+ * @access public
+ */
+class OpenStreetMap {
+	nome = "Test"
+}
