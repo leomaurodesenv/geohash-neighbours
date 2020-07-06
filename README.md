627437542493be077268889fd23edfd690b6ea6c
Leonardo Moraes <leo.mauro.desenv@gmail.com>
2020-07-05 21:25:58 -0300

    add badges, codacy fixes

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # geohash-neighbours
 
+[![MIT license](https://img.shields.io/badge/MIT-license-blue.svg)](LICENSE.md)
+[![GitHub](https://img.shields.io/badge/Code-GitHub-yellow.svg)](https://github.com/leomaurodesenv/geohash-neighbours)
+[![Codacy Badge](https://app.codacy.com/project/badge/Grade/9072bb5427ca4ce091ad7f169d3ad901)](https://www.codacy.com/manual/leomaurodesenv/geohash-neighbours?utm_source=github.com&amp;utm_medium=referral&amp;utm_content=leomaurodesenv/geohash-neighbours&amp;utm_campaign=Badge_Grade)
+
 ---
 
 "A geohash is a convenient way of expressing a location (anywhere in the world) using a short alphanumeric string, with greater precision obtained with longer strings" by [Geohashes](https://www.movable-type.co.uk/scripts/geohash.html).  
--- js/openstreetmap.js
+++ js/openstreetmap.js
@@ -14,45 +14,45 @@ const baseFixed = 14;
 
 class OpenStreetMap {
 
-	/**
+    /**
      * @method module:OpenStreetMap::getMapSource
      * Return the map source
-     * @arg {Float} latitude 		Latitude center
-     * @arg {Float} longitude		Longitude center
-     * @arg {Float} latDistance    	Distance from center for latitude
+     * @arg {Float} latitude        Latitude center
+     * @arg {Float} longitude       Longitude center
+     * @arg {Float} latDistance     Distance from center for latitude
      * @arg {Float} longDistance    Distance from center for longitude
      * @access public
      * @return {String}
      */
-	static getMapSource(latitude, longitude, latDistance, longDistance) {
-		return baseUrl + "?bbox="
-			+ (longitude + longDistance).toFixed(baseFixed) + "%2C"
-			+ (latitude + latDistance).toFixed(baseFixed) + "%2C" 
-			+ (longitude - longDistance).toFixed(baseFixed) + "%2C"
-			+ (latitude - latDistance).toFixed(baseFixed) + "&amp;layer=mapnik";
-	}
+    static getMapSource(latitude, longitude, latDistance, longDistance) {
+        return baseUrl + "?bbox="
+            + (longitude + longDistance).toFixed(baseFixed) + "%2C"
+            + (latitude + latDistance).toFixed(baseFixed) + "%2C" 
+            + (longitude - longDistance).toFixed(baseFixed) + "%2C"
+            + (latitude - latDistance).toFixed(baseFixed) + "&amp;layer=mapnik";
+    }
 
-	/**
+    /**
      * @method module:OpenStreetMap::getIframe
      * Return the map iframe
-     * @arg {String} mapSource 		Map source
+     * @arg {String} mapSource         Map source
      * @access public
      * @return {String}
      */
-	static getIframe(width, height, id, mapSource) {
-		/* creating element */
-		var elem = document.createElement("iframe");
-		elem.setAttribute("width", width);
-		elem.setAttribute("height", height);
-		elem.setAttribute("id", id);
-		elem.setAttribute("src", mapSource);
-		/* default settings */
-		elem.setAttribute("frameborder", "0");
-		elem.setAttribute("scrolling", "no");
-		elem.setAttribute("marginheight", "0");
-		elem.setAttribute("marginwidth", "0");
-		return elem;
-	}
+    static getIframe(width, height, id, mapSource) {
+        /* creating element */
+        var elem = document.createElement("iframe");
+        elem.setAttribute("width", width);
+        elem.setAttribute("height", height);
+        elem.setAttribute("id", id);
+        elem.setAttribute("src", mapSource);
+        /* default settings */
+        elem.setAttribute("frameborder", "0");
+        elem.setAttribute("scrolling", "no");
+        elem.setAttribute("marginheight", "0");
+        elem.setAttribute("marginwidth", "0");
+        return elem;
+    }
 }
 
 /* 
